d6863c0f56d92935671235a56cc09e41cac5f57d
= <gohelbhargav401@gmail.com>
2025-12-18 02:36:34 +0530

    ADD: post routes added

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,12 +1,14 @@
 import express from "express"
 import { ensureUserAuthenticate,checkTokenExists } from "../middleware/auth.middleware.js"
-import { newPostCreation } from "../controller/post.controller.js"
+import { allPosts, getOnlyUserPosts, newPostCreation } from "../controller/post.controller.js"
 const postRouter = express.Router()
 
 postRouter.use(checkTokenExists)
 postRouter.use(ensureUserAuthenticate)
 
 postRouter.post("/create-post",newPostCreation)
+postRouter.get("/all-posts",allPosts)
+postRouter.get("/login-user-posts",getOnlyUserPosts)
 
 
 export default postRouter

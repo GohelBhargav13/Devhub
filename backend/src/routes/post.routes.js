57f2cb3e90b23fe95e56829a349919ca6a0bf3f4
= <gohelbhargav401@gmail.com>
2025-12-19 00:01:57 +0530

    UPDATE: post & user routes are updated

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { ensureUserAuthenticate,checkTokenExists } from "../middleware/auth.middleware.js"
-import { allPosts, getOnlyUserPosts, newPostCreation } from "../controller/post.controller.js"
+import { allPosts, deletePostOfUser, getOnlyUserPosts, newPostCreation } from "../controller/post.controller.js"
 const postRouter = express.Router()
 
 postRouter.use(checkTokenExists)
@@ -9,6 +9,7 @@ postRouter.use(ensureUserAuthenticate)
 postRouter.post("/create-post",newPostCreation)
 postRouter.get("/all-posts",allPosts)
 postRouter.get("/login-user-posts",getOnlyUserPosts)
+postRouter.delete("/delete-post/:postId",deletePostOfUser)
 
 
 export default postRouter
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getProfile, userLogin, userRegister } from "../controller/user.controller.js"
+import { getProfile, userLogin, userLogout, userRegister } from "../controller/user.controller.js"
 import { checkTokenExists, ensureUserAuthenticate } from "../middleware/auth.middleware.js"
 const userRouter = express.Router()
 
@@ -7,5 +7,6 @@ const userRouter = express.Router()
 userRouter.post("/register",userRegister)
 userRouter.post("/login",userLogin)
 userRouter.get("/user-porfile",checkTokenExists,ensureUserAuthenticate,getProfile)
+userRouter.get("/user-logout",checkTokenExists,ensureUserAuthenticate,userLogout)
 
 export default userRouter

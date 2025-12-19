ecd5b8ab287ca5eb9383c55afaee4c9a3d1454b1
= <gohelbhargav401@gmail.com>
2025-12-19 18:07:26 +0530

    UPDATE: user model update with new values

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,11 +1,23 @@
-import { pgTable,uuid,varchar,text,timestamp } from "drizzle-orm/pg-core"
+import {
+  pgTable,
+  uuid,
+  varchar,
+  text,
+  timestamp,
+  boolean,
+  pgEnum
+} from "drizzle-orm/pg-core";
 
-export const userTable = pgTable("users",{
-    user_id: uuid().primaryKey().defaultRandom(),
-    user_name: varchar({ length: 255 }).notNull(),
-    user_email: varchar({ length: 25 }).unique().notNull(),
-    user_password: text().notNull(),
-    salt: text().notNull(),
-    internal_username: varchar({ length: 255 }).notNull(),
-    created_at: timestamp().defaultNow()
-})
+export const userRoleEnum = pgEnum("user_role", ["ADMIN", "USER"]);
+
+export const userTable = pgTable("users", {
+  user_id: uuid().primaryKey().defaultRandom(),
+  user_name: varchar({ length: 255 }).notNull(),
+  user_email: varchar({ length: 255 }).unique().notNull(),
+  user_password: text().notNull(),
+  salt: text().notNull(),
+  internal_username: varchar({ length: 255 }).notNull(),
+  is_active: boolean().default(false),
+  user_role: userRoleEnum().default("USER").notNull(),
+  created_at: timestamp().defaultNow().notNull(),
+});

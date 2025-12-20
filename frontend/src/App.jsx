8627c47fecef748c4fcd6894fa497753b56ede78
= <gohelbhargav401@gmail.com>
2025-12-20 23:37:50 +0530

    ADD: app routes are updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,48 @@ import { Routes,Route } from "react-router-dom"
 import LoginPage from "./pages/loginPage.jsx"
 import { Toaster } from "react-hot-toast"
 import HomePage from "./pages/HomePage.jsx"
+import RegisterPage from "./pages/RegisterPage.jsx"
+import { useAuthStore } from "./store/auth.store.js"
+import { useEffect } from "react"
+import { ProtectRoute } from "./services/ProtectRoute.jsx"
+import UserProfile from "./pages/UserProfile.jsx"
+import NewPost from "./pages/NewPost.jsx"
+
 
 function App() {
+  const initializeAuth = useAuthStore((state) => state.initializeAuth)
+  const userData = useAuthStore((state) => state.userData)
+  useEffect(() => {
+    initializeAuth()
+  },[])
+
   return (
     <>
       <div className='min-h-screen bg-linear-to-br from-slate-950 via-slate-900 to-slate-950 text-white'>
-        <Toaster position="bottom-left" />
+        <Toaster position="bottom-left" reverseOrder={false} containerStyle={{
+          animation:"ease-in-out",
+          accentColor:"AccentColor"
+        }} 
+        toastOptions={
+          {
+            style:{
+              backgroundColor: "darkblue",
+              color:"white",
+              padding:'16px',
+              border: '1px solid cyan'
+            },
+            iconTheme:{
+              primary:'cyan',
+              secondary:'gray'
+            },
+          }
+        } />
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={ <ProtectRoute> <HomePage /> </ProtectRoute>} />
           <Route path="/login" element={ <LoginPage /> } />
+          <Route path="/register" element={ <RegisterPage /> } />
+          <Route path="/get-profile" element={ <ProtectRoute> <UserProfile /> </ProtectRoute> } />
+          <Route path="/new-post" element={ <ProtectRoute> <NewPost /> </ProtectRoute> } />
         </Routes>
       </div>
     </>

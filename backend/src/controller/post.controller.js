5907969faae5d6071e7befd8c35034228ffcb703
= <gohelbhargav401@gmail.com>
2025-12-26 22:52:37 +0530

    fix: post_tags features

--- backend/src/controller/post.controller.js
+++ backend/src/controller/post.controller.js
@@ -9,14 +9,14 @@ import { fetchAllPosts,loginUserPosts,deleteAUserPost } from "../services/post.s
 // New-post controller
 export const newPostCreation = async(req,res) => {
     try {
-        const { post_description,post_link } = req.body
+        const { post_description,post_link,post_tags } = req.body
 
-        if(!post_description || !post_link){
+        if(!post_description || !post_link || !post_tags){
             return res.status(400).json({ StatusCode:400, message: "All fields are required" })
         }
 
         const user_id = req.user.user_id
-        const {status,new_post} = await addNewPost(post_description,post_link,user_id)
+        const {status,new_post} = await addNewPost(post_description,post_link,user_id,post_tags)
 
         if(!status){
             return res.status(400).json({ 'StatusCode':400, 'error': "New Post creation is failed" })

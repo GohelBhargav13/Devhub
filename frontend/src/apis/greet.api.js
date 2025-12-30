1ea9887d438f92d3f6bab5308f22627702179cbd
= <gohelbhargav401@gmail.com>
2025-12-30 09:38:34 +0530

    fix: user greet is added

--- frontend/src/apis/greet.api.js
+++ frontend/src/apis/greet.api.js
@@ -4,13 +4,13 @@ export const userGreetMessage = (username) => {
         const hours = date.getHours(); // Get current hour in 24-hour format
 
         if (hours >= 5 && hours < 12) {
-            return `Good Morning ${username}🌄`;
+            return `A fresh start and Good Morning ${username}🌄`;
         } else if (hours >= 12 && hours < 17) {
             return `Good Afternoon ${username}🌆`;
         } else if (hours >= 17 && hours < 21) {
             return `Good Evening ${username}🌆`;
         } else {
-            return `Late Night ${username}🌚`;
+            return `it's actually late night ${username}, get some rest!🌃`;
         }
 
     } catch (error) {
